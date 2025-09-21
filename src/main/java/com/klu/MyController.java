b2ab577e555cd71022167c03b9a58e3fbe926d2f
Sathvik31060 <163741485+Sathvik31060@users.noreply.github.com>
2025-09-21 15:25:19 +0530

    Add files via upload

---
+++ src/main/java/com/klu/MyController.java
@@ -0,0 +1,17 @@
+package com.klu;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/back2")
+public class MyController {
+
+    @GetMapping
+    public String test() {
+        return "Hello from /back2!";
+    }
+}
+
